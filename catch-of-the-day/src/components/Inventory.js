cb6ab8159418f6073c5cd9dedfd336ba4330ae07
Kevin Davis <osfan501@gmail.com>
2017-01-05 15:29:42 +0100

    first commit

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Inventory extends Component {
+  render() {
+    return (
+      <div>
+        <p>Inventory</p>
+      </div>
+    );
+  }
+}
+
+export default Inventory;
