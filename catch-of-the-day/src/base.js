d8ea5efb0c52e44f39fb2868253ddf46c74da69f
Kevin Davis <osfan501@gmail.com>
2017-01-07 01:09:31 +0100

    sync inventory with firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,5 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({apiKey: "AIzaSyAXFdnYYjGghpHu6qMPhJCKvYEgZifEoTQ", authDomain: "catch-of-the-day-de420.firebaseapp.com", databaseURL: "https://catch-of-the-day-de420.firebaseio.com", storageBucket: "catch-of-the-day-de420.appspot.com", messagingSenderId: "1035043181855"});
+
+export default base;
--- catch-of-the-day/src/components/app.jsx
+++ catch-of-the-day/src/components/app.jsx
@@ -4,6 +4,8 @@ import Order from './Order'
 import Inventory from './Inventory'
 import Fish from './Fish'
 import sampleFishes from '../sample-fishes'
+import base from '../base';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -25,6 +27,18 @@ class App extends Component {
       order: {}
     };
   }
+
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
   addFish(fish) {
     //update state
     const fishes = {
@@ -50,6 +64,7 @@ class App extends Component {
     //update state
     this.setState({order})
   }
+
   render() {
     return (
       <div className='catch-of-the-day'>
